61a1965e6eb5a87c5f83b5031963dcf5a7024329
mrszeeshan <139902365+mrszeeshan@users.noreply.github.com>
2023-08-08 20:42:58 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person : string = "Quaid-E-Azam";
+let massage: string = famous_person  + ` once said ,
+" I don't take the right decisions , I take decisions and make them right"`;
+
+console.log (massage);
